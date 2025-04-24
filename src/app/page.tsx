b68dd5afdda11732a2050690e6b7405d5a43ac2e
amank55 <amankumarr2005ak@gmail.com>
2025-04-24 20:29:47 +0530

    feat: add UserPrograms component and integrate it into HomePage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import TerminalOverlay from "@/components/TerminalOverlay";
 import { Button } from "@/components/ui/button";
+import UserPrograms from "@/components/UserProgram";
 //import UserPrograms from "@/components/UserPrograms";
 import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
@@ -115,7 +116,7 @@ const HomePage = () => {
           </div>
         </div>
       </section>
-
+      <UserPrograms />
    
     </div>
   );
